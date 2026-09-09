365aaeb4bb2477a98cca66bf83d36834c7d2bac3
Shaik Jaheer Basha <jaheer121@gmail.com>
2026-09-10 00:11:23 +0530

    Add files via upload
    Week1/Day 2/data Types Assignment

---
+++ dataTypes.js
@@ -0,0 +1,22 @@
+/* Datatype specify the type of data the variable holds
+
+Primitive data types: it 
+*/
+
+var firstName ="Jaheer Basha"
+
+var companyName ="hp"
+
+var mobileNumber =7218126839
+
+var isAutomation =true
+
+var hasPlaywright
+
+console.log(firstName + ", "+typeof(firstName));
+console.log(companyName + ", "+typeof(companyName));
+console.log(mobileNumber + ", "+typeof(mobileNumber));
+console.log(isAutomation + ", "+typeof(isAutomation));
+console.log(hasPlaywright + ", "+typeof(hasPlaywright));
+
+
